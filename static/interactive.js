853e41e848a81fdcc5f73cc3adb0370b4dd78096
Giulio Caflisch <129767311+giuliocaflisch@users.noreply.github.com>
2024-06-10 19:35:10 +0200

    Update interactive.js

--- static/interactive.js
+++ static/interactive.js
@@ -1,1 +1,9 @@
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+}
 
+function Run() { 
+	//function to make the text bold using DOM method 
+	document.getElementById("textarea1").style.fontWeight = "bold"; 
+}
